6047cd4b6f964dca18f5d363f1b27647980cd408
Michael S. Klishin <michael@novemberain.com>
2012-02-05 03:55:28 +0400

    Add one more test, extract duplication

---
+++ src/test/java/com/mulesoft/quartz/mongo/MongoDBJobStoreStoringTriggersWithCronScheduleTest.java
@@ -0,0 +1,55 @@
+package com.mulesoft.quartz.mongo;
+
+import com.mongodb.DBCollection;
+import org.junit.Before;
+import org.junit.Test;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.spi.OperableTrigger;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.quartz.CalendarIntervalScheduleBuilder.calendarIntervalSchedule;
+import static org.quartz.TriggerBuilder.newTrigger;
+
+/*
+ * $Id$
+ * --------------------------------------------------------------------------------------
+ * Copyright (c) Michael S. Klishin
+ *
+ * The software in this package is published under the terms of the Apache License, Version 2.0
+ * license, a copy of which has been included with this distribution in the
+ * LICENSE.txt file.
+ */
+
+public class MongoDBJobStoreStoringTriggersWithCronScheduleTest extends MongoDBJobStoreTest {
+  @Test
+  public void testJobStorageUsingCronSchedule() throws Exception {
+    assertEquals(0, jobsCollection.count());
+    assertEquals(0, store.getNumberOfJobs());
+
+    JobDetail job = JobBuilder.newJob()
+        .storeDurably()
+        .usingJobData("rowId", 10)
+        .withIdentity("shaveAllYaks", "yakCare")
+        .build();
+
+    this.getStore().storeJob(job, false);
+    assertEquals(1, jobsCollection.count());
+    assertEquals(1, store.getNumberOfJobs());
+
+    OperableTrigger trigger = (OperableTrigger) newTrigger()
+        .withIdentity("yakShavingSchedule", "yakCare")
+        .forJob(job)
+        .startNow()
+        .withSchedule(calendarIntervalSchedule().withIntervalInHours(1))
+        .build();
+
+    assertEquals(0, triggersCollection.count());
+    assertEquals(0, store.getNumberOfTriggers());
+
+    this.getStore().storeTrigger(trigger, false);
+    assertEquals(1, triggersCollection.count());
+    assertEquals(1, store.getNumberOfTriggers());
+  }
+}
--- src/test/java/com/mulesoft/quartz/mongo/MongoDBJobStoreTest.java
+++ src/test/java/com/mulesoft/quartz/mongo/MongoDBJobStoreTest.java
@@ -3,33 +3,28 @@ package com.mulesoft.quartz.mongo;
 import com.mongodb.DB;
 import com.mongodb.DBCollection;
 import com.mongodb.Mongo;
-import org.junit.Assert;
 import org.junit.Before;
-import org.junit.Test;
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.ObjectAlreadyExistsException;
 import org.quartz.simpl.SimpleClassLoadHelper;
-import org.quartz.spi.OperableTrigger;
 
-import java.util.Date;
+public class MongoDBJobStoreTest {
+  protected MongoDBJobStore store;
+  protected DBCollection jobsCollection;
+  protected DBCollection triggersCollection;
 
-import static org.quartz.SimpleScheduleBuilder.repeatMinutelyForever;
-import static org.quartz.TriggerBuilder.newTrigger;
+  public MongoDBJobStoreTest() {
+  }
 
-/*
- * $Id$
- * --------------------------------------------------------------------------------------
- * Copyright (c) MuleSoft, Inc.  All rights reserved.  http://www.mulesoft.com
- *
- * The software in this package is published under the terms of the CPAL v1.0
- * license, a copy of which has been included with this distribution in the
- * LICENSE.txt file.
- */
+  public DBCollection getJobsCollection() {
+    return jobsCollection;
+  }
 
-public class MongoDBJobStoreTest extends Assert {
+  public MongoDBJobStore getStore() {
+    return store;
+  }
 
-  private MongoDBJobStore store;
+  public DBCollection getTriggersCollection() {
+    return triggersCollection;
+  }
 
   @Before
   public void setUpJobStore() throws Exception {
@@ -44,72 +39,7 @@ public class MongoDBJobStoreTest extends Assert {
     store.setDbName("quartz");
     store.setAddresses("localhost");
     store.initialize(new SimpleClassLoadHelper(), null);
-  }
-
-  @Test
-  public void testJobStorage() throws Exception {
-    final DBCollection jobsCollection = store.getJobCollection();
-    assertEquals(0, jobsCollection.count());
-    assertEquals(0, store.getNumberOfJobs());
-
-    JobDetail job = JobBuilder.newJob()
-        .storeDurably()
-        .usingJobData("key", "value")
-        .withIdentity("name", "group")
-        .build();
-
-    store.storeJob(job, false);
-    assertEquals(1, jobsCollection.count());
-    assertEquals(1, store.getNumberOfJobs());
-
-    try {
-      store.storeJob(job, false);
-      fail("Expected jobs collection to have a duplicate");
-    } catch (ObjectAlreadyExistsException e) {
-      // expected
-    }
-
-    OperableTrigger trigger = (OperableTrigger) newTrigger()
-        .withIdentity("name", "group")
-        .forJob(job)
-        .startAt(new Date())
-        .withSchedule(repeatMinutelyForever())
-        .build();
-
-    final DBCollection triggersCollection = store.getTriggerCollection();
-    assertEquals(0, triggersCollection.count());
-    assertEquals(0, store.getNumberOfTriggers());
-
-    store.storeTrigger(trigger, false);
-    assertEquals(1, triggersCollection.count());
-    assertEquals(1, store.getNumberOfTriggers());
-
-    try {
-      store.storeTrigger(trigger, false);
-      fail("Should not be able to store the same trigger twice");
-    } catch (ObjectAlreadyExistsException e) {
-      // expected
-    }
-
-    OperableTrigger trigger2 = (OperableTrigger) newTrigger()
-        .withIdentity("name2", "group")
-        .forJob(job)
-        .startAt(new Date())
-        .withSchedule(repeatMinutelyForever())
-        .build();
-
-    store.storeTrigger(trigger2, false);
-    assertEquals(2, triggersCollection.count());
-    assertEquals(2, store.getNumberOfTriggers());
-
-    JobDetail job2 = store.retrieveJob(job.getKey());
-    assertEquals("name", job2.getKey().getName());
-    assertEquals("group", job2.getKey().getGroup());
-    assertEquals(1, job2.getJobDataMap().size());
-    assertEquals("value", job2.getJobDataMap().get("key"));
-
-    trigger2 = store.retrieveTrigger(trigger.getKey());
-    assertEquals("name", trigger2.getKey().getName());
-    assertEquals("group", trigger2.getKey().getGroup());
+    jobsCollection = store.getJobCollection();
+    triggersCollection = store.getTriggerCollection();
   }
 }
