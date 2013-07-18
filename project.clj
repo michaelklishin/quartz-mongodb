b95eee21b2f5644d2a1cb054cf0e764077552e55
Michael Klishin <michael@defprotocol.org>
2013-07-18 22:08:23 +0400

    1.3.0-beta2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/quartz-mongodb "1.3.0-beta2-SNAPSHOT"
+(defproject com.novemberain/quartz-mongodb "1.3.0-beta2"
   :description "A MongoDB-backed store for Quartz Scheduler and Quartzite"
   :min-lein-version "2.0.0"
   :license {:name "Apache License 2.0"}
