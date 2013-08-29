1cb552390f8cde672e9d85db7fad22fbc08332f8
Illyr <david.regnier13@gmail.com>
2013-08-29 11:23:53 +0200

    Test commit

---
+++ src/main/java/com/novemberain/quartz/mongodb/Test.java
@@ -0,0 +1,13 @@
+package com.novemberain.quartz.mongodb;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
