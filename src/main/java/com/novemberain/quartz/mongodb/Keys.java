bae87137eed7d1551a3e583ec89d8f8886a00643
Sven Boekhoff <sven@boekhoff.info>
2015-04-13 15:19:04 +0200

    Fixing Checkstyle issues: HideUtilityClassConstructor
    "Utility classes should not have a public or default constructor."
    
    http://checkstyle.sourceforge.net/config_design.html#HideUtilityClassConstructor

--- src/main/java/com/novemberain/quartz/mongodb/Keys.java
+++ src/main/java/com/novemberain/quartz/mongodb/Keys.java
@@ -11,6 +11,8 @@ public class Keys {
   public static final String KEY_NAME = "keyName";
   public static final String KEY_GROUP = "keyGroup";
 
+  private Keys() {}
+  
   // backwards compatibility...  Remove these in next 2.0 release.
   @Deprecated
   public static final String JOB_KEY_NAME = KEY_NAME;
