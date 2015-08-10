66359cddde8d2ba77a3d5a5669634ce904feee31
Przemysław Wojnowski <esperanto@cumego.com>
2015-08-10 13:39:13 +0200

    Remove deprecated Keys consts

--- src/main/java/com/novemberain/quartz/mongodb/Keys.java
+++ src/main/java/com/novemberain/quartz/mongodb/Keys.java
@@ -11,20 +11,6 @@ public class Keys {
   public static final String KEY_NAME = "keyName";
   public static final String KEY_GROUP = "keyGroup";
 
-  // backwards compatibility...  Remove these in next 2.0 release.
-  @Deprecated
-  public static final String JOB_KEY_NAME = KEY_NAME;
-  @Deprecated
-  public static final String JOB_KEY_GROUP = KEY_GROUP;
-  @Deprecated
-  public static final String TRIGGER_KEY_NAME = KEY_NAME;
-  @Deprecated
-  public static final String TRIGGER_KEY_GROUP = KEY_GROUP;
-  @Deprecated
-  public static final String LOCK_KEY_NAME = KEY_NAME;
-  @Deprecated
-  public static final String LOCK_KEY_GROUP = KEY_GROUP;
-
   public static BasicDBObject keyToDBObject(Key<?> key) {
     BasicDBObject job = new BasicDBObject();
     job.put(KEY_NAME, key.getName());
