13f636b1b790888894d262d9ce4002c142e1b1d3
Przemysław Wojnowski <esperanto@cumego.com>
2016-03-30 17:59:31 +0200

    Test that active schedulers are not removed.

--- src/test/clojure/com/novemberain/quartz/mongodb/cluster/recoverer_test.clj
+++ src/test/clojure/com/novemberain/quartz/mongodb/cluster/recoverer_test.clj
@@ -11,19 +11,32 @@
   (Scheduler. "name" id lastCheckinTime 0))
 
 (defn- ^SchedulerDao mock-scheduler-dao
-  []
+  [lastCheckinTime1 lastCheckinTime2 removals]
   ;; Dummy args are needed to fulfill super ctor needs:
   (proxy [SchedulerDao] [nil "sn" "in" 0 nil]
     (getAllByCheckinTime []
-      [(create-scheduler "id1" 1)
-       (create-scheduler "id2" 8000)])
+      [(create-scheduler "id1" lastCheckinTime1)
+       (create-scheduler "id2" lastCheckinTime2)])
     (remove [name instanceId lastCheckinTime]
-      (assert (= name "name") (str "Was: " name))
-      (assert (= instanceId "id1") (str "Was: " instanceId))
-      (assert (= lastCheckinTime 1) (str "Was: " lastCheckinTime)))))
+      (swap! removals conj {:name name
+                            :instance-id instanceId
+                            :last-checkin-time lastCheckinTime}))))
+
+(deftest should-not-remove-active-schedulers
+  (let [removals (atom [])
+        clock (utils/const-clock 1)
+        scheduler-dao (mock-scheduler-dao 1 2 removals)
+        recoverer (Recoverer. scheduler-dao clock)]
+    (.recover recoverer)
+    (is (empty? @removals))))
 
 (deftest should-remove-defunct-schedulers
-  (let [clock (utils/const-clock 9000)
-        scheduler-dao (mock-scheduler-dao)
+  (let [removals (atom [])
+        clock (utils/const-clock 10000)
+        scheduler-dao (mock-scheduler-dao 1 10000 removals)
         recoverer (Recoverer. scheduler-dao clock)]
-    (.recover recoverer)))
+    (.recover recoverer)
+    (is (= (count @removals) 1))
+    (is (= (-> @removals first :name) "name"))
+    (is (= (-> @removals first :instance-id) "id1"))
+    (is (= (-> @removals first :last-checkin-time) 1))))
