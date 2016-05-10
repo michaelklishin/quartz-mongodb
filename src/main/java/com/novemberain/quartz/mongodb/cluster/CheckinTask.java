116546623734455191611822b23abe7c8740453f
Przemysław Wojnowski <esperanto@cumego.com>
2016-05-10 14:18:50 +0200

    Stop JVM when cannot check-in on time.
    If a scheduler cannot register itself due to an exception we stop JVM to
    prevent concurrent execution of the same jobs together with other nodes
    that might have found this scheduler as defunct and take over its
    triggers.

--- src/main/java/com/novemberain/quartz/mongodb/cluster/CheckinTask.java
+++ src/main/java/com/novemberain/quartz/mongodb/cluster/CheckinTask.java
@@ -12,10 +12,31 @@ public class CheckinTask implements Runnable {
 
     private static final Logger log = LoggerFactory.getLogger(CheckinTask.class);
 
+    /**
+     * This implementation shuts down JVM to not allow to execute the same JOB by two schedulers.
+     *
+     * If a scheduler cannot register itself due to an exception we stop JVM to prevent
+     * concurrent execution of the same jobs together with other nodes that might have found this
+     * scheduler as defunct and take over its triggers.
+     */
+    private static final Runnable DEFAULT_ERROR_HANDLER = new Runnable() {
+        @Override
+        public void run() {
+            //TODO Is there a way to stop only Quartz?
+            System.exit(1);
+        }
+    };
+
     private SchedulerDao schedulerDao;
+    private Runnable errorhandler = DEFAULT_ERROR_HANDLER;
 
     public CheckinTask(SchedulerDao schedulerDao) {
         this.schedulerDao = schedulerDao;
+    }
+
+    // for tests only
+    public void setErrorHandler(Runnable errorhandler) {
+        this.errorhandler = errorhandler;
     }
 
     @Override
@@ -24,8 +45,8 @@ public class CheckinTask implements Runnable {
         try {
             schedulerDao.checkIn();
         } catch (MongoException e) {
-            //TODO what to do in case of errors? Stop Quartz?
             log.error("Node " + schedulerDao.instanceId + " could not check-in: " + e.getMessage(), e);
+            errorhandler.run();
         }
     }
 }
--- src/test/clojure/com/novemberain/quartz/mongodb/cluster/checkin_task_test.clj
+++ src/test/clojure/com/novemberain/quartz/mongodb/cluster/checkin_task_test.clj
@@ -1,22 +1,32 @@
 (ns com.novemberain.quartz.mongodb.cluster.checkin-task-test
   (:require [clojure.test :refer :all])
-  (:import com.novemberain.quartz.mongodb.cluster.CheckinTask
+  (:import com.mongodb.MongoException
+           com.novemberain.quartz.mongodb.cluster.CheckinTask
            com.novemberain.quartz.mongodb.dao.SchedulerDao))
 
 (defn- ^SchedulerDao create-scheduler-dao
-  [checkin-counter]
+  [checkin-fn]
   ;; Dummy args are needed to fulfill super ctor needs:
   (proxy [SchedulerDao] [nil "sn" "in" 0 nil]
-    (checkIn [] (swap! checkin-counter inc))))
+    (checkIn [] (checkin-fn))))
 
 (defn- create-task
-  [checkin-counter]
-  (CheckinTask. (create-scheduler-dao checkin-counter)))
+  [checkin-fn]
+  (CheckinTask. (create-scheduler-dao checkin-fn)))
 
 (deftest should-store-scheduler-data-to-checkin
   (let [checkin-counter (atom 0)
-        checkinTask (create-task checkin-counter)]
-    (.run checkinTask)
+        checkin-task (create-task #(swap! checkin-counter inc))]
+    (.run checkin-task)
     (is (= @checkin-counter 1))
-    (.run checkinTask)
+    (.run checkin-task)
     (is (= @checkin-counter 2))))
+
+(deftest should-stop-scheduler-when-hit-by-exception
+  (let [error-counter (atom 0)
+        checkin-fn #(throw (MongoException. "Checkin Error!"))
+        checkin-task (create-task checkin-fn)]
+    (.setErrorHandler checkin-task
+                      (reify Runnable (run [_] (swap! error-counter inc))))
+    (.run checkin-task)
+    (is (= @error-counter 1))))
