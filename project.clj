6708c1dfb0d0edf981a49a0410d689d70a586b96
Michael Klishin <michael@clojurewerkz.org>
2016-06-20 23:00:38 +0300

    Back to dev version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject com.novemberain/quartz-mongodb "2.0.0-rc2"
+(defproject com.novemberain/quartz-mongodb "2.0.0-rc3-SNAPSHOT"
   :description "A MongoDB-backed store for Quartz Scheduler and Quartzite"
   :min-lein-version "2.4.2"
   :license {:name "Apache License 2.0"}
