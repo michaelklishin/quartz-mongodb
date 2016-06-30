94482142b02a8238624590dd4b42df5c67f8a63c
Przemysław Wojnowski <esperanto@cumego.com>
2016-07-01 00:07:44 +0200

    LockDao tests moved to Spock. utils.clj and mongo.clj moved to Groovy.

---
+++ src/test/groovy/com/novemberain/quartz/MongoHelper.groovy
@@ -0,0 +1,112 @@
+package com.novemberain.quartz
+
+import com.mongodb.MongoClient
+import com.mongodb.MongoClientOptions
+import com.mongodb.WriteConcern
+import com.mongodb.client.MongoCollection
+import com.mongodb.client.MongoDatabase
+import org.bson.Document
+
+class MongoHelper {
+
+    static testDatabaseName = 'quartz_mongodb_test'
+
+    static MongoClient client = new MongoClient('localhost',
+            MongoClientOptions.builder()
+                    .writeConcern(WriteConcern.JOURNALED)
+                    .build())
+
+    static MongoDatabase testDatabase = client.getDatabase(testDatabaseName)
+
+    static Map<String,MongoCollection<Document>> collections = [
+            calendars: testDatabase.getCollection('quartz_calendars'),
+            locks: testDatabase.getCollection('quartz_locks'),
+            jobs: testDatabase.getCollection('quartz_jobs'),
+            jobGroups: testDatabase.getCollection('quartz_paused_job_groups'),
+            schedulers: testDatabase.getCollection('quartz_schedulers'),
+            triggers: testDatabase.getCollection('quartz_triggers'),
+            triggerGroups: testDatabase.getCollection('quartz_paused_trigger_groups')
+    ]
+
+    static def clearColl(String colKey) {
+        collections[colKey].deleteMany(new Document())
+    }
+
+    static def purgeCollections() {
+        //Remove all data from Quartz MongoDB collections.
+        clearColl('triggers')
+        clearColl('jobs')
+        clearColl('locks')
+        clearColl('calendars')
+        clearColl('schedulers')
+        clearColl('triggerGroups')
+        clearColl('jobGroups')
+    }
+
+    /**
+     * Adds a new scheduler entry created from given map.
+     */
+    static def addScheduler(Map dataMap) {
+        collections['schedulers'].insertOne(new Document(dataMap))
+    }
+
+    /**
+     * Adds a new Job entry created from given map.
+     */
+    static def addJob(Map dataMap) {
+        collections['jobs'].insertOne(new Document(dataMap))
+    }
+
+    /**
+     * Adds a new Lock entry created from given map.
+     */
+    static def addLock(Map dataMap) {
+        collections['locks'].insertOne(new Document(dataMap))
+    }
+
+    /**
+     * Adds a new Trigger entry created from given map.
+     */
+    static def addTrigger(Map dataMap) {
+        collections['triggers'].insertOne(new Document(dataMap))
+    }
+
+    /**
+     * Return number of elements in a collection.
+     */
+    static def getCount(String col) {
+        collections[col].count()
+    }
+
+    /**
+     * Return locks collection as MongoCollection.
+     */
+    static def getLocksColl() {
+        collections['locks']
+    }
+
+    /**
+     * Return schedulers collection as MongoCollection.
+     */
+    static def getSchedulersColl() {
+        collections['schedulers']
+    }
+
+    /**
+     * Return the first document from given collection.
+     */
+    static def getFirst(String col) {
+        getFirst(col, [:])
+    }
+
+    static def getFirst(String col, Map amap) {
+        collections[col].find(new Document(amap)).first()
+    }
+
+    /**
+     * Return all documents from given collection.
+     */
+    static def Collection<Document> findAll(String col) {
+        collections[col].find(new Document()).into([])
+    }
+}
---
+++ src/test/groovy/com/novemberain/quartz/dao/LocksDaoTest.groovy
@@ -0,0 +1,155 @@
+package com.novemberain.quartz.dao
+
+import com.mongodb.MongoWriteException
+import com.novemberain.quartz.Clocks
+import com.novemberain.quartz.MongoHelper
+import com.novemberain.quartz.mongodb.dao.LocksDao
+import com.novemberain.quartz.mongodb.util.Clock
+import com.novemberain.quartz.mongodb.util.Keys
+import org.quartz.TriggerKey
+import spock.lang.Shared
+import spock.lang.Specification
+
+import java.util.concurrent.atomic.AtomicInteger
+
+class LocksDaoTest extends Specification {
+
+    @Shared def instanceId = 'locksDaoTestId'
+    def testClock = Clocks.incClock()
+
+    def setup() {
+        MongoHelper.purgeCollections()
+    }
+
+    def 'should have passed collection and instanceId'() {
+        given:
+        def locksCollection = MongoHelper.getLocksColl()
+        def dao = new LocksDao(locksCollection, testClock, instanceId)
+
+        expect:
+        dao.instanceId == instanceId
+        dao.collection.is(locksCollection)
+    }
+
+    def 'should lock trigger'() {
+        given:
+        def clock = Clocks.incClock()
+        def dao = createDao(clock)
+        def tkey = new TriggerKey('n1', 'g1')
+
+        when:
+        dao.lockTrigger(tkey)
+
+        then:
+        def locks = MongoHelper.findAll('locks')
+        locks.size() == 1
+        assertLock(locks.first(), instanceId, 1)
+    }
+
+    def 'should throw an exception when locking trigger again'() {
+        // There should be no possibility to lock a trigger again.
+        given:
+        def dao = createDao()
+        def triggerKey = new TriggerKey('n1', 'g1')
+
+        when:
+        dao.lockTrigger(triggerKey)
+
+        then:
+        notThrown(MongoWriteException)
+
+        when:
+        dao.lockTrigger(triggerKey)
+
+        then:
+        thrown(MongoWriteException)
+        def locks = MongoHelper.findAll('locks')
+        locks.size() == 1
+        locks.first().get(Keys.KEY_NAME) == 'n1'
+        locks.first().get(Keys.KEY_GROUP) == 'g1'
+    }
+
+    def 'should relock trigger when found'() {
+        given:
+        def counter = new AtomicInteger(0)
+        def clock = Clocks.incClock(counter)
+        def otherId = 'defunct scheduler'
+        def triggerKey = new TriggerKey('n1', 'g1')
+        def dao = createDao(clock, otherId)
+
+        when: 'Lock using other scheduler in time "1"'
+        dao.lockTrigger(triggerKey)
+
+        then:
+        assertOneLock(otherId, 1)
+        counter.get() == 1
+
+        when: 'Using the new scheduler relock lock with time "1"'
+        dao = createDao(clock)
+        def relocked = dao.relock(triggerKey, new Date(counter.get()))
+
+        then:
+        relocked
+
+        and: 'Still one lock, but with updated owner and time'
+        assertOneLock(instanceId, 2)
+        counter.get() == 2
+    }
+
+    def 'should not relock when already changed'() {
+        // Don't relock when other scheduler have already done that.
+        given:
+        def counter = new AtomicInteger(0)
+        def clock = Clocks.incClock(counter)
+        def tkey = new TriggerKey('n1', 'g1')
+        def otherId = 'defunct scheduler'
+        def otherDao = createDao(clock, otherId)
+
+        when: 'Lock using other scheduler in time 1'
+        otherDao.lockTrigger(tkey)
+
+        then: 'Update the lock with new time 2'
+        otherDao.relock(tkey, new Date(counter.get()))
+        counter.get() == 2
+
+        when: 'Using the new scheduler try to relock with old time 1'
+        def relocked = createDao(clock).relock(tkey, new Date(counter.get()-1))
+
+        then:
+        !relocked
+
+        and: 'Still one lock, updated be previous scheduler'
+        assertOneLock(otherId, 2)
+        counter.get() == 3
+    }
+
+    def createDao() {
+        createDao(testClock)
+    }
+
+    def createDao(Clock clock) {
+        createDao(clock, instanceId)
+    }
+
+    def createDao(Clock clock, String id) {
+        new LocksDao(MongoHelper.getLocksColl(), clock, id)
+    }
+
+    def void assertLock(lock, instanceId, time) {
+        assertLock(lock, 'n1', 'g1', instanceId, time)
+    }
+
+    def void assertLock(lock, key, group, instanceId, time) {
+        assert lock.get(Keys.LOCK_TYPE) == Keys.LockType.t.name()
+        assert lock.get(Keys.KEY_NAME) == key
+        assert lock.get(Keys.KEY_GROUP) == group
+        assert lock.get('instanceId') == instanceId
+        assert lock.get('time').getTime() == time
+    }
+
+    def void assertOneLock(String instanceId, long time) {
+        def locks = MongoHelper.findAll('locks')
+        locks.size() == 1
+        assertLock(locks.first(), instanceId, time)
+    }
+}
